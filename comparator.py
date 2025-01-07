ef0db61a65ad8246627a7ec693c95c037d7670a0
Melissa BELKESSAM <melissabelkessambelkessam@gmail.com>
2025-01-07 11:06:12 +0100

    project initialization

---
+++ comparator.py
@@ -0,0 +1,11 @@
+# Python Program to find largest of Two Numbers
+
+a = float(input("Please Enter the First value a : "))
+b = float(input("Please Enter the Second Value b : "))
+
+if(a>b):
+   print("{0} is greater than {1}".format(a,b))
+elif(b>a):
+   print("{0} is greater than {1}".format(b,a))
+else:
+   print("Both a and b are Equal")
